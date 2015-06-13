e82af1a897236f9b4c46b7c33cce681e27c95c36
sunchengcheng <sun@chengcheng.info>
2015-06-14 00:13:21 +0800

    add const vars in header file.

--- app/include/push.h
+++ app/include/push.h
@@ -10,6 +10,23 @@
 
 #include <c_types.h>
 
+
+enum CONN_STATUS {
+	STATUS_CONNECTING = 0,
+	STATUS_DNS_LOOKUP = 1,
+	STATUS_CONNECTED = 2,
+	STATUS_DISCONNECTED = 3
+};
+
+
+
+enum PUSH_VERSION {
+	VER_AT = 0,
+	VER_NODEMCU = 1,
+	VER_SDK = 2,
+};
+
+
 typedef void(*msg_cb)(uint8* pdata, uint32 len);
 
 void ICACHE_FLASH_ATTR push_register(uint32 appid, char appkey[32], msg_cb msgcb);
